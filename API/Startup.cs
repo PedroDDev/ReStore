f9f584ba0f6291400a73871b87c6c05b1a307efe
PedroHenriqueDiasBatista <phdiasbatistaprofessional@gmail.com>
2023-01-15 05:53:46 -0300

    feat: create React project

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,7 @@ namespace API
             {
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
 
         }
 
@@ -45,6 +46,10 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(options => {
+                options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
 
             app.UseAuthorization();
 
---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,45 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import Avatar from "@mui/material/Avatar/Avatar";
+import CardHeader from "@mui/material/CardHeader/CardHeader";
+import { Product } from "../../app/models/product";
+
+interface Props {
+    product: Product;
+}
+
+export default function ProductCard({product}: Props){
+    return(
+    <Card>
+        <CardHeader 
+            avatar={
+                <Avatar sx={{bgcolor: 'secondary.main'}}>
+                    {product.name.charAt(0).toUpperCase()}
+                </Avatar>
+            }
+            title={product.name}
+            titleTypographyProps={{
+                sx: {fontWeight: "bold", color: "primary.main"}
+            }}
+        />
+        <CardActionArea>
+            <CardMedia
+            sx={{height: 140, backgroundSize: "contain", bgcolor: "primary.light"}}
+            image={product.pictureUrl}
+            title={product.name}
+            />
+            <CardContent>
+            <Typography gutterBottom color="secondary" variant="h5">
+                R${(product.price / 100).toFixed(2)}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                {product.brand} / {product.type}
+            </Typography>
+            </CardContent>
+        </CardActionArea>
+        <CardActions>
+            <Button size="small" color="primary">Add to cart</Button>
+            <Button size="small" color="primary">More</Button>
+        </CardActions>
+    </Card>
+    )
+}
